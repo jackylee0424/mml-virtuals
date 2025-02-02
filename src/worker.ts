565ca9e81a1a7563ae265915ebb7221774a51ec3
jackie lee <jackylee0424@users.noreply.github.com>
2025-02-02 07:36:10 -0800

    add concurrency check from firebase

--- src/worker.ts
+++ src/worker.ts
@@ -5,17 +5,22 @@ import {
   replyToTweetFunction,
   postTweetFunction,
 } from "./functions";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const helloWorker = new GameWorker({
   id: "hello_worker",
   name: "hello worker",
-  description: "has the ability to say hello",
+  description: "has the ability to feel competitive based on the concurrency",
   functions: [helloFunction],
   getEnvironment: async () => {
+    const res = await fetch(`${process.env.FIREBASE_FUNCTION_URI}/getagents?passcode=${process.env.FIREBASE_PASSCODE}`)
+    const resJson = await res.json();
+    const concurrency = resJson["concurrency"];
     return {
-      status: "friendly",
+      status: concurrency > 1 ? "competitive": "friendly",
       // Add any environment variables your worker needs
-      someLimit: 10,
+      concurrency: concurrency,
     };
   },
 });
